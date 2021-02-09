0c6e315136ddc219c2ffc6e1685e21071ff31059
alenSavov <alensavov@gmail.com>
2021-02-09 17:11:53 +0200

    added EpisodeDetails component

--- rickandmorty-app/src/components/EpisodeDetails.js
+++ rickandmorty-app/src/components/EpisodeDetails.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-function EpisodeDetails() {
+function EpisodeDetails({ match }) {
+
+    useEffect(() => {
+        fetchEpisode();
+    }, [])
+
+    const [episode, setItem] = useState({});
+    const [charactersId, setCharactersId] = useState([]);
+    const [characters, setCharacters] = useState([]);
+
+    const fetchEpisode = async () => {
+        const fetchEpisode = await fetch(`https://rickandmortyapi.com/api/episode/${ match.params.id }`);
+        const episode = await fetchEpisode.json();
+        setItem(episode);
+
+        const characterIdCollection = [];
+        episode.characters.forEach(e => {
+            characterIdCollection.push(e.split('/')[5]);
+        });
+
+        let result = characterIdCollection.map(i => Number(i));
+
+        console.log(result);
+
+        const fetchCharacters = async () => {
+            const res = await fetch(`https://rickandmortyapi.com/api/character/[${ result }]`);
+
+            const characters = res.json();
+            console.log(res);
+        }
+
+        fetchCharacters();
+    }
+
     return (
         <div>
-            DETAILS
+            {characters.map(c => (
+                <div key={c.id}>
+                    <h1>{c.name}</h1>
+                    <p>{c.status}</p>
+                    <p>{c.species}</p>
+                    <p>{c.gender}</p>
+                </div>
+            ))}
         </div>
     )
 }
 
-export default EpisodeDetails
+export default EpisodeDetails;
