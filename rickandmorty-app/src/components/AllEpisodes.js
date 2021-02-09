b8fcfd12d6a6a9dddd243b0396bb7c2b4e4ee0a4
alenSavov <alensavov@gmail.com>
2021-02-09 17:09:41 +0200

    added all episodes component

--- rickandmorty-app/src/components/AllEpisodes.js
+++ rickandmorty-app/src/components/AllEpisodes.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "../../src/index.css";
+
 
 function AllEpisodes() {
-    return (
-        <div>
-            ALLL
-        </div>
-    )
+  const [episodes, setEpisodes] = useState([]);
+
+  useEffect(() => {
+    fetchEpisodes();
+  }, []);
+
+  const fetchEpisodes = async () => {
+    const res = await fetch(
+      'https://rickandmortyapi.com/api/episode/?page=2'
+    );
+
+    const episodes = await res.json();
+    setEpisodes(episodes.results);
+  }
+
+  return (
+    <div className='App'>
+      <ul className='list-group mb-4'>
+        {episodes.map(e => (
+          <li key={e.id} characters={e.characters} className='list-group-item'>
+            <Link to={`/allepisodes/${ e.id }`}>{e.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default AllEpisodes
+export default AllEpisodes;
