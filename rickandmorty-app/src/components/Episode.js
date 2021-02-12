8cdb7ab37af044cf8580cb5a923980e304afbc7f
alenSavov <alensavov@gmail.com>
2021-02-12 14:13:23 +0200

    episode fix

--- rickandmorty-app/src/components/Episode.js
+++ rickandmorty-app/src/components/Episode.js
@@ -1,63 +1,71 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { makeStyles, Typography } from "@material-ui/core";
 
-import CharacterCard from './CharacterCard';
-import EpisodeDetailsTemplate from './EpisodeDetailsTemplate';
-import CharacterCardExtendInfo from './CharacterCardExtendInfo';
+import CharacterCard from "./CharacterCard";
+import EpisodeDetailsTemplate from "./EpisodeDetailsTemplate";
+import CharacterCardExtendInfo from "./CharacterCardExtendInfo";
 
 const useStyles = makeStyles(() => ({
-    
-    characterWrapper: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexWrap: 'wrap',
-        width: '100%',
-    },
-    character: {
-        background: 'green',
-    },
+  characterWrapper: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexWrap: "wrap",
+    width: "100%",
+  },
+  character: {
+    background: "green",
+  },
 }));
 
 function Episode({ match }) {
-    useEffect(() => {
-        fetchEpisode();
-        fetchCharacters();
-    }, [])
+  const classes = useStyles();
+  const [episode, setEpisode] = useState({});
+  const [charactersId, setCharactersId] = useState([]);
+  const [characters, setCharacters] = useState([]);
 
-    const classes = useStyles();
-    const [episode, setEpisode] = useState({});
-    const [charactersId, setCharactersId] = useState([]);
-    const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchEpisode();
+  }, []);
 
-    const fetchEpisode = async () => {
-        const fetchEpisode = await fetch(`https://rickandmortyapi.com/api/episode/${ match.params.id }`);
-        const episode = await fetchEpisode.json();
-        setEpisode(episode);
-
-        const characterIdCollection = [];
-        episode.characters.forEach(e => {
-            characterIdCollection.push(e.split('/')[5]);
-        });
-
-        setCharactersId(characterIdCollection);
+  useEffect(() => {
+    if (charactersId.length > 0) {
+      fetchCharacters();
     }
+  }, [charactersId]);
 
-    const fetchCharacters = async () => {
-        const fetchCharacters = await fetch(`https://rickandmortyapi.com/api/character/${ charactersId }`);
-        const characters = await fetchCharacters.json();
+  const fetchEpisode = async () => {
+    const fetchEpisode = await fetch(
+      `https://rickandmortyapi.com/api/episode/${match.params.id}`
+    );
+    const episode = await fetchEpisode.json();
+    setEpisode(episode);
 
-        setCharacters(characters.results);
-    }
+    const characterIdCollection = [];
+    episode.characters.forEach((e) => {
+      characterIdCollection.push(e.split("/")[5]);
+    });
 
-    return (
-        <>           
-            <div className={classes.characterWrapper}>
-                <EpisodeDetailsTemplate episode={episode} />
-                <CharacterCard character={characters} isExtended={true} />
-            </div>
-        </>
-    )
+    setCharactersId(characterIdCollection);
+  };
+
+  const fetchCharacters = async () => {
+    const fetchCharacters = await fetch(
+      `https://rickandmortyapi.com/api/character/${charactersId}`
+    );
+    const characters = await fetchCharacters.json();
+
+    setCharacters(characters);
+  };
+  
+  return (
+    <>
+      <div className={classes.characterWrapper}>
+        <EpisodeDetailsTemplate episode={episode} />
+        <CharacterCard character={characters} isExtended={true} />
+      </div>
+    </>
+  );
 }
 
 export default Episode;
